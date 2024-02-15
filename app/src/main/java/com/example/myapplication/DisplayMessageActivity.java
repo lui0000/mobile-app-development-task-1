6a52dc20368e01856f72c456e7c2eda1159c68e7
Lisa <elbondarenko04@gmail.com>
2024-02-15 19:51:19 +0300

    Completing work with a new activity added automatically

--- app/src/main/java/com/example/myapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/example/myapplication/DisplayMessageActivity.java
@@ -2,13 +2,26 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+        // Шаг 2: Удаление строки setContentView(R.layout.activity_display_message);
+        // Шаг 3: Создание переменной типа Intent и присвоение ей значения getIntent()
+        Intent intent = getIntent();
+        // Шаг 5: Получение дополнительных данных строкового типа из MyActivity
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        // Создание объекта TextView
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        // Делаем textView корневым элементов разметки activity
+        setContentView(textView);
+
     }
 }
